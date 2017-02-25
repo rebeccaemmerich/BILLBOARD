cc0d2623dc4006aaa1c9e2461cf687dead3ae91e
rebeccaemmerich <rebeccaemmerich@gmail.com>
2017-02-25 22:53:07 +0200

    added login authentication

--- views.py
+++ views.py
@@ -8,16 +8,20 @@ from .forms import PostForm
 from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Title
+from django.contrib.auth.decorators import login_required
 
+
+
+@login_required
 def index(request):
     return render(request, 'billboard/page.html')
 
-
+@login_required
 def post_list(request):
     pl=Title.objects.all
     return render(request,'billboard/page.html', {"posts": pl})
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -32,11 +36,12 @@ def post_new(request):
         form = PostForm()
     return render(request, 'billboard/post_edit.html', {'form': form})
 
-
+@login_required
 def post_edit(request):
     return render(request, 'billboard/page.html')
 
-
+@login_required
 def post_detail(request):
     form = PostForm()
     return render(request, 'billboard/page.html', {'form': form})
+
